a388f9bf8285a499466a0eecf569a615b09a0dc1
FernandoMattos16 <fernandopinheiromattos@gmail.com>
2023-09-13 18:07:04 -0300

    Projeto 3

--- Projeto 3/client/aplicacao.py
+++ Projeto 3/client/aplicacao.py
@@ -13,58 +13,57 @@ import math
 #use uma das 3 opcoes para atribuir à variável a porta usada
 #serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
 #serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
-serialName = "COM3"                  # Windows(variacao de)
+serialName = "COM5"                  # Windows(variacao de)
 
 def main():
     try:
-        print("Main iniciada")
         com1 = enlace(serialName)
     
         com1.enable()
 
-        print("Comunicação aberta com sucesso!")
+        print("Comunicação aberta com sucesso!\n")
         
         # Handshake
         while True:
             # Enviando byte de sacrifício
+            print("Enviando handshake")
             time.sleep(.2)
-            print("Enviando handshake\n")
             com1.sendData(b'00')
-            print("Aguardando retorno para inicio de transmissão . . .\n")
+            print(". . . Aguardando retorno para inicio de transmissão . . .\n")
             rxBuffer, nRx = com1.getData(2)
-            if rxBuffer == 'S':
+            if rxBuffer == 'S' or rxBuffer == 's':
                 print("Reiniciando tentativa de transmissão\n")
                 continue
-            if rxBuffer == 'N':
-                print("Tentativa de transmissão interrompida\nEncerrando comunicação . . .\n")
+            if rxBuffer == 'N' or rxBuffer == 'n':
+                print("\nTentativa de transmissão interrompida\n. . . Encerrando comunicação . . .\n")
                 com1.disable()
                 sys.exit("Comunicação encerrada")
             else:
                 com1.rx.clearBuffer()
-                time.sleep(.1)
-                print("Handshake confirmado!\nIniciando transmissão . . .\n")
+                time.sleep(1)
+                print("Handshake confirmado!\n. . . Iniciando transmissão . . .\n")
                 break
          
-        pathImageTx = "./img/luna.jpg"
+        pathImageTx = "Projeto 3/img/logo-insper.png"
         ImageTx = open(pathImageTx, 'rb').read()
         print(ImageTx)
 
         lenImage = len(ImageTx)
-        nPacotes = math.ceil(lenImage/114)
+        nPacotes = math.ceil(lenImage/50)
         nPacotesBytes = nPacotes.to_bytes(2, byteorder="big")
 
         print(f"Serão enviados {nPacotes} pacotes!\n")
         com1.sendData(nPacotesBytes)
         time.sleep(1)
 
-        payloads = [ImageTx[i:i + 114] for i in range(0, len(ImageTx), 114)]
+        payloads = [ImageTx[i:i + 50] for i in range(0, len(ImageTx), 50)]
 
         contPacotes = 0
         while contPacotes < nPacotes:
             print(f"Enviando o {contPacotes+1}º pacote")
             # HEAD
-            nPacote = (contPacotes+1).to_bytes(5, byteorder="big") # Número do pacote
-            tamPayload = (len(payloads[contPacotes])).to_bytes(5, byteorder="big") # Tamanho do pacote
+            nPacote = (contPacotes+1).to_bytes(6, byteorder="big") # Número do pacote
+            tamPayload = (len(payloads[contPacotes])).to_bytes(6, byteorder="big") # Tamanho do pacote
             HEAD = nPacote + tamPayload
             # PAYLOAD
             PAYLOAD = payloads[contPacotes] # Pacote
@@ -83,14 +82,12 @@ def main():
             if confirmacao == b'2':
                 pacoteCerto, pacoteCertoLen = com1.getData(2)
                 contPacotes = int.from_bytes(pacoteCerto, "big") - 1
-                print(f"Inconsistência no EOP! Reenviando {contPacotes+1}º pacote")
-                print(contPacotes)
+                print(f"Inconsistência no EOP! Reenviando {contPacotes+1}º pacote\n")
 
             elif confirmacao == b'1':
                 pacoteCerto, pacoteCertoLen = com1.getData(2)
                 contPacotes = int.from_bytes(pacoteCerto, "big") - 1
-                print(f"A sequência do pacote está incorreta! Reenviando {contPacotes+1}º pacote")
-                print(contPacotes)
+                print(f"A sequência do pacote está incorreta! Reenviando {contPacotes+1}º pacote\n")
 
             else:
                 contPacotes += 1
