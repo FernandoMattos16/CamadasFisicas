040e1995150e717a29facfdba9fe336c73813ce8
FernandoMattos16 <fernandopinheiromattos@gmail.com>
2023-09-20 18:18:16 -0300

    Projeto 4
    Co-authored-by: Pedro Dutra <pabdutra@users.noreply.github.com>

---
+++ Projeto 4/client/aplicacao.py
@@ -0,0 +1,147 @@
+from enlace import *
+import time
+import sys
+import math
+from datetime import datetime
+
+
+
+def createLog(data, tipo):
+    tempo = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    tipoMsg = data[0]
+    tamMsg = len(data)
+    pacoteEnviado = data[4]
+    totalPacotes = data[3]
+    log = f"{tempo} / {tipo} / {tipoMsg} / {tamMsg} / {pacoteEnviado} / {totalPacotes}\n"
+    return log
+
+# Voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal :
+#   python -m serial.tools.list_ports
+# Se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411" # Mac    (variacao de)
+serialName = "COM5"                  # Windows(variacao de)
+
+def main():
+    try:
+        path = "Projeto 4/assets/img/logo-insper.jpeg"  
+        file = open(path, 'rb').read()
+        logs = ''
+
+        # * INICIALIZANDO CLIENT
+        com1 = enlace(serialName)
+    
+        com1.enable()
+
+        print("Comunicação aberta com sucesso!\n")
+
+        # * HANDSHAKE
+        print("Iniciando HandShake\n")
+        timeMax = time.time()
+        while True:
+            payload, h1, h2, h3, h4, h5, h6, h7, h8, h9 = b'\x00'
+            h0 = 0
+            head = h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9
+            eop = 0x00000000.to_bytes(4, byteorder="big")
+            pacote = head + payload + eop
+            com1.sendData(pacote)
+            logs += createLog(pacote, 'envio')
+            time.sleep(.5)
+            confirmacao, lenConfimacao = com1.getData(15)
+            timeF = time.time()
+            if timeF - timeMax >= 25:
+                print("Servidor não respondeu após quarta tentativa. Cancelando comunicação.")
+                com1.disable()
+                sys.exit("Comunicação encerrada")
+            elif type(confirmacao) == str:
+                print(confirmacao)
+            elif type(confirmacao) == None:
+                com1.disable()
+                sys.exit("Comunicação encerrada")
+            else:
+                print("Handshake confirmado!\n. . . Iniciando transmissão . . .\n")
+                break
+                
+        # * ENVIO DOS PACOTES
+        print("Agora vamos realizar o início do envio dos pacotes\n")
+        payloads = [file[i:i + 114] for i in range(0, len(file), 114)]
+        # h3 = quantidade total de pacotes
+        lenImage = len(file)
+        h3 = math.ceil(lenImage/114)
+        h3 = (h3).to_bytes(1, byteorder="big")
+        # h4 = número do pacote sendo enviado
+        h4 = 1
+        # último pacote enviado com sucesso
+        cont = 0
+        while cont < int.from_bytes(h3, "big"):
+            print(f"Enviando informações do pacote {h4}")
+            h4 = (h4).to_bytes(1, byteorder="big")
+            h7 = (h4-1).to_bytes(1, byteorder="big")
+            h0 = (3).to_bytes(1, byteorder="big")
+            h5 = len(payloads[int.from_bytes(h4,"big")-1])
+            h5 = (h5).to_bytes(1, byteorder="big")
+            head = h0 + h1 + h2 + h3 + h4 + h5 + h6 + h7 + h8 + h9
+            pacote = head + payloads[int.from_bytes(h4, "big") - 1] + eop
+            
+            timeMax = time.time()
+            while True:
+                com1.sendData(pacote)
+                logs += createLog(pacote, 'envio')
+                time.sleep(.5)
+                confirmacao, lenConfimacao = com1.getData(15)
+                timeF = time.time()
+                if timeF - timeMax >= 25:
+                    print("Servidor não respondeu após quarta tentativa. Cancelando comunicação.")
+                    com1.disable()
+                    sys.exit("Comunicação encerrada")
+                elif type(confirmacao) == str:
+                    print(confirmacao)
+                elif type(confirmacao) == None:
+                    com1.disable()
+                    sys.exit("Comunicação encerrada")
+                else:
+                    print("Handshake confirmado!\n. . . Iniciando transmissão . . .\n")
+                    break
+
+            if confirmacao[0] == 4:
+                logs += createLog(confirmacao, 'recebimento')
+                print(confirmacao[7])
+                print("Tudo certo! O servidor recebeu o pacote corretamente.")
+            else:
+                logs += createLog(confirmacao, 'recebimento')
+                numPacoteCorreto = confirmacao[7]
+                print(f"Uhmm, algo deu errado no envio :( Precisamos reenviar o pacote {numPacoteCorreto}")
+            
+            if numPacoteCorreto is None:
+                if h4 == 2:
+                    h4 += 2
+                    cont +=1
+                else:
+                    h4 += 1
+                    cont += 1
+            else:
+                h4 = numPacoteCorreto
+                cont = numPacoteCorreto - 1
+
+        with open(f'Projeto 4/assets/log/log.txt', 'w') as f:
+            f.write(logs)
+        # * FECHANDO CLIENT
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com1.disable()
+        exit()
+        
+
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        com1.disable()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
---
+++ Projeto 4/server/aplicacao.py
@@ -0,0 +1,154 @@
+from enlace import *
+import time
+from datetime import datetime
+from datetime import datetime
+
+
+
+def createLog(data, tipo):
+    tempo = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    tipoMsg = data[0]
+    tamMsg = len(data)
+    pacoteEnviado = data[4]
+    totalPacotes = data[3]
+    logs = f"{tempo} / {tipo} / {tipoMsg} / {tamMsg} / {pacoteEnviado} / {totalPacotes}\n"
+    return logs
+# Voce deverá descomentar e configurar a porta com através da qual ira fazer comunicaçao
+#   para saber a sua porta, execute no terminal:
+#   python -m serial.tools.list_ports
+# Se estiver usando windows, o gerenciador de dispositivos informa a porta
+
+#use uma das 3 opcoes para atribuir à variável a porta usada
+#serialName = "/dev/ttyACM0"           # Ubuntu (variacao de)
+#serialName = "/dev/tty.usbmodem1411"  # Mac    (variacao de)
+serialName = "COM3"                    # Windows(variacao de)
+
+def main():
+    try:
+        com1 = enlace(serialName)
+        logs = ''
+
+        com1.enable()
+
+        print("Comunicação aberta com sucesso!\n")
+
+        
+
+        # HANDSHAKE
+        print(". . . Esperando Handshake . . .\n")
+
+        pacote, lenPacote = com1.getData(15)
+
+        logs += createLog(pacote, 'recebimento')
+
+        pacote = list(pacote)
+        pacote = list(map(int, pacote))
+        pacote[0] = 2
+        responseHandShake = b''
+        for i in pacote:
+            i = (i).to_bytes(1, byteorder="big")
+            responseHandShake += i
+
+        print(". . . Handshake recebido com sucesso . . . Enviando reposta de estabilidade.")
+
+        com1.sendData(pacote)
+        time.sleep(.5)
+
+        # Recebendo Pacotes
+        data = b''
+        numPacote = 1
+        while True:
+            print(f". . . Recebendo o {numPacote}º pacote . . .\n")
+            #HEAD
+            head, lenHead = com1.getDataServer(10)
+            lenPayload = head[5]
+            payload_EOP, lenPayload_EOP = com1.getDataServer(lenPayload + 4)
+
+            logs += createLog(pacote, 'recebimento')
+            head = pacote[0:10]
+            h0 = head[0] # tipo de mensagem
+            h1 = head[1] # livre
+            h2 = head[2] # livre
+            h3 = head[3] # número total de pacotes do arquivo
+            h4 = head[4] # número do pacote sendo enviado
+            h5 = head[5] # se tipo for handshake:id do arquivo; se tipo for dados: tamanho do payload
+            h6 = head[6] # pacote solicitado para recomeço quando a erro no envio.
+            h7 = head[7] # último pacote recebido com sucesso.
+            h8 = head[8] # CRC
+            h9 = head[9] # CRC
+
+            numPacoteRecebido = h4
+
+            # Checando se o número do pacote enviado está correto
+            if h4 != numPacote:
+                print(f"O número do pacote está errado! Por favor reenvie o pacote {numPacote}")
+                h0 = 6
+                h7 = numPacote
+                confirmacao = [h0, h1, h2, h3, h4, h5, h6, h7, h8, h9]
+                responseCorrectMsg = b''
+                for i in confirmacao:
+                    i = (i).to_bytes(1, byteorder="big")
+                    responseCorrectMsg += i
+                com1.sendData(responseCorrectMsg + b'\x00' + 0x00000000.to_bytes(4, byteorder="big"))
+                time.sleep(.5)
+                break
+
+            # Checando se o EOP está no local correto
+            eop = pacote[len(pacote)-4:len(pacote)+1]
+            if eop != 0x00000000.to_bytes(4, byteorder="big"):
+                print(f"O eop está no local errado! Por favor reenvie o pacote {numPacote}")
+                break
+            
+        
+            print("Está tudo certo com a mensagem! Vamos enviar uma mensagem de confirmação.")
+            h0 = 4
+            h7 = numPacote
+            confirmacao = [h0, h1, h2, h3, h4, h5, h6, h7, h8, h9]
+            responseCorrectMsg = b''
+            for i in confirmacao:
+                i = (i).to_bytes(1, byteorder="big")
+                responseCorrectMsg += i
+            com1.sendData(responseCorrectMsg + b'\x00' + 0x00000000.to_bytes(4, byteorder="big"))
+            logs += createLog(responseCorrectMsg + b'\x00' + 0x00000000.to_bytes(4, byteorder="big"), 'envio')
+            time.sleep(.5)
+            
+            numPacote = h4
+        
+            if numPacote == numPacoteRecebido:
+                numPacote += 1
+                data += payload_EOP[0:len(payload_EOP) - 4]                
+
+                if numPacote == h3 + 1:
+                    data += payload_EOP[0:len(payload_EOP) - 4]
+                    break
+
+
+        pathImageRx = "Projeto 4/assets/img/rxImage.png"
+        f = open(pathImageRx, 'wb')
+        f.write(data)
+        f.close()
+
+        with open(f"Projeto 4/assets/log/log.txt", 'w') as f:
+            f.write(logs)
+        # * FECHANDO CLIENT
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com1.disable()
+        exit()
+        
+    except Exception as erro:
+        print("ops! :-\\")
+        print(erro)
+        print("-------------------------")
+        print("Comunicação encerrada")
+        print("-------------------------")
+        com1.disable()
+        exit()
+        
+
+    #so roda o main quando for executado do terminal ... se for chamado dentro de outro modulo nao roda
+if __name__ == "__main__":
+    main()
+
+        
